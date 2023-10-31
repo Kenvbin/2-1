92161a13921ebb5f295593a7f5be83eb9eff3cb8
Kenvbin <gixxer5506@yahoo.com>
2023-10-30 21:05:57 -0700

    2-1

---
+++ src/PersonalInfo.java
@@ -0,0 +1,11 @@
+// Project 2-1 Program that displays, name, address, and phone number
+
+public class PersonalInfo{
+
+  public static void main(String[] args) {
+
+    System.out.println("Name: Kevin Fischer");
+    System.out.println("Address: 12345 That One St");
+    System.out.println("Phone Number: (310) 123-4567");
+  }
+}
